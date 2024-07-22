9289e0fbc5fb37929bc554d14bfb3eefb8957f6c
Muhammad usama Qureshi <umuhammadusamaqureshi@gmail.com>
2024-07-22 17:55:05 +0000

    day01 complete

---
+++ day01.js
@@ -0,0 +1,52 @@
+
+// VARAIBLE AND DATATYPES
+//TASK/ACTIVITIES
+
+// Activity 1: varaible declation
+// task 1
+ var age = 20;
+ console.log(age);
+ 
+ // task 02
+
+ let name = 'Jonh';
+ console.log(name);
+
+ //Activity 2: consntant declaration
+ 
+ // task 03  
+ const isLoggedIn = true  // initially false hoga 
+ console.log(isLoggedIn);
+
+//Activity 3:  Data types
+
+// Task 04:
+
+ var price = 500;  // number
+ var college = 'Muet'; // string 
+ var status = false  // online or offline  // boolean 
+ var student={ // object
+     name:'Ali',
+     age : 18,
+     address: 'Hyderabad'
+ };
+
+ var listOfFruits = ['apple', 'banana', 'peach','graps', 'mango']; // array
+
+console.log(typeof(listOfFruits));
+
+//Activity 4: Reassinging the varaibles values
+
+// Task 05:  using let
+
+ let city = 'karachi';
+ city = 'jamshoro';
+ console.log(city);
+
+ //Activity 5: understanding const
+
+ // Task 06:
+  
+ const idCardNumber = 819871771721
+ idCardNumber = 1092019212791729 // here you cant reassign the const varaible
+ console.log(idCardNumber);
